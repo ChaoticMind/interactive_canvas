2e07a3de253f666e79f183f2673b34fbae5ef668
Kevin Azzam <kevin@azz.am>
2015-04-10 18:04:56 +0200

    Implemented interactive canvas following http://simonsarris.com/blog/510-making-html5-canvas-useful

--- canvas/js/main.js
+++ canvas/js/main.js
@@ -3,5 +3,11 @@ window.onload = initialize;
 // http://simonsarris.com/blog/510-making-html5-canvas-useful
 
 function initialize() {
-	// do nothing
+	var s = new CanvasState(document.getElementById('canvas'));
+
+	s.addShape(new Shape(40,40,50,50)); // The default is gray
+	s.addShape(new Shape(60,140,40,60, 'lightskyblue'));
+	// Lets make some partially transparent
+	s.addShape(new Shape(80,150,60,30, 'rgba(127, 255, 212, .5)'));
+	s.addShape(new Shape(125,80,30,80, 'rgba(245, 222, 179, .7)'));
 }
